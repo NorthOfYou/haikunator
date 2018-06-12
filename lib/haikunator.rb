87b8d1c2a51e0dc3756949879d5d03c3551f409d
Ben Govero <ben.govero@gmail.com>
2018-06-12 11:34:45 -0500

    Removed some words that could result in names that could result in support calls
    Let’s just leave it at that

--- lib/haikunator.rb
+++ lib/haikunator.rb
@@ -29,14 +29,14 @@ module Haikunator
 
     def adjectives
       %w(
-        autumn hidden bitter misty silent empty dry dark summer
-        icy delicate quiet white cool spring winter patient
+        autumn hidden misty silent empty dry dark summer
+        icy delicate quiet cool spring winter patient
         twilight dawn crimson wispy weathered blue billowing
         broken cold damp falling frosty green long late lingering
         bold little morning muddy old red rough still small
-        sparkling throbbing shy wandering withered wild black
-        young holy solitary fragrant aged snowy proud floral
-        restless divine polished ancient purple lively nameless
+        sparkling shy wandering withered wild
+        solitary fragrant aged snowy proud floral
+        restless polished ancient lively
       )
     end
 
@@ -48,7 +48,7 @@ module Haikunator
         bush dew dust field fire flower firefly feather grass
         haze mountain night pond darkness snowflake silence
         sound sky shape surf thunder violet water wildflower
-        wave water resonance sun wood dream cherry tree fog
+        wave water resonance sun dream cherry tree fog
         frost voice paper frog smoke star
       )
     end
